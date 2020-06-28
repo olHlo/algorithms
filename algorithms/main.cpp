9894d671ec6ebeaf5f03fbf5c53dd4c1045a13a9
neen <olHlo@outlook.com>
2020-06-28 23:21:11 +0800

    Initial Commit

---
+++ algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithms
+//
+//  Created by 永文闫 on 2020/6/28.
+//  Copyright © 2020年 lemonair. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
