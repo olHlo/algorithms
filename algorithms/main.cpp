5421633c0aa272cfc36af512574fb03f0a355df4
neen <olHlo@outlook.com>
2020-06-28 23:30:52 +0800

    array list part.1

--- algorithms/main.cpp
+++ algorithms/main.cpp
@@ -1,15 +1,52 @@
 //
-//  main.cpp
-//  algorithms
-//
-//  Created by 永文闫 on 2020/6/28.
+//  Created by 闫永文 on 2020/5/18.
 //  Copyright © 2020年 lemonair. All rights reserved.
 //
 
-#include <iostream>
+// C 实现线性表 - 1. array list
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include "stdio.h"
+
+#include "stdlib.h"
+
+#define MAX_SIZE 20
+
+typedef int ElementType;
+
+// list的数据结构
+typedef struct LNode *List;
+struct LNode{
+    ElementType Data[MAX_SIZE];
+    int Last;
+};
+
+// 初始化 - 创建一张空表
+List MakeEmpty()
+{
+    List Ptrl;
+    Ptrl = (List)malloc(sizeof(struct LNode));
+    Ptrl->Last = -1;
+    return Ptrl;
+}
+
+// 查找
+int Find(ElementType X, List Ptrl)
+{
+    int i = 0;
+    while(i <= Ptrl->Last && Ptrl->Data[i]!=X)
+        i++;
+    if (i > Ptrl->Last) return -1;
+    else return i;
+}
+
+// 插入
+void Insert()
+{
+    
+}
+
+// list action
+int main()
+{
+    
 }
